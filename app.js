cdbea4353ac3d87f44a89da7f6ad698f96d1ab5b
Damian Ciasnocha <daneek80@gmail.com>
2020-06-12 21:05:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 // targetting 'add a new todo section - .add class'
-const add = document.querySelector('.add');
+const add = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const generateTemplate = todo => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${todo}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>
+    `;
+  list.innerHTML += html;
+};
 // telling what todo after 'new todo' added
-add.addEventListener('submit', e => {
-    e.preventDefault();
-    const todo = add.add.value.trim();
-
-
+add.addEventListener("submit", e => {
+  e.preventDefault();
+  const todo = add.add.value.trim();
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
 });
